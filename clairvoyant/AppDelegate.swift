bf45d73a13095ae45e7a74106f178724cff60e70
Joseph Sungpil Choi <chousemath@gmail.com>
2017-12-31 22:05:55 +0900

    First

---
+++ clairvoyant/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  clairvoyant
+//
+//  Created by Joseph Sungpil Choi on 31/12/2017.
+//  Copyright © 2017 Joseph Sungpil Choi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ clairvoyant/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  clairvoyant
+//
+//  Created by Joseph Sungpil Choi on 31/12/2017.
+//  Copyright © 2017 Joseph Sungpil Choi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
